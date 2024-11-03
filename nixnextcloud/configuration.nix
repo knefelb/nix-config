967c5a9416e19963209dab09937f7b905ad6e0be
knefelb <knefelb@gmail.com>
2024-11-03 15:23:24 -0600

    Updated config files with proper configs

--- nixnextcloud/configuration.nix
+++ nixnextcloud/configuration.nix
@@ -4,47 +4,7 @@
 
 { config, lib, pkgs, ... }:
 {
-  #begin Nvidia Config
-  # Enable OpenGL
-  hardware.graphics = {
-    enable = true;
-  };
 
-  # Load nvidia driver for Xorg and Wayland
-  services.xserver.videoDrivers = ["nvidia"];
-
-  hardware.nvidia = {
-
-    # Modesetting is required.
-    modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead 
-    # of just the bare essentials.
-    powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
-    powerManagement.finegrained = false;
-
-    # Use the NVidia open source kernel module (not to be confused with the
-    # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
-    # Only available from driver 515.43.04+
-    # Currently alpha-quality/buggy, so false is currently the recommended setting.
-    open = false;
-
-    # Enable the Nvidia settings menu,
-	  # accessible via `nvidia-settings`.
-    nvidiaSettings = true;
-
-    # Optionally, you may need to select the appropriate driver version for your specific GPU.
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
-  };
-   ##nvidia Config End---------
 
   imports =
     [ # Include the results of the hardware scan.
@@ -55,7 +15,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "nixosDesktop"; # Define your hostname.
+  networking.hostName = "NextCloudNix"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -93,14 +53,6 @@
   # Configure keymap in X11
   services.xserver.xkb.layout = "us";
 
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
-  services.avahi = {
-  enable = true;
-  nssmdns4 = true;
-  openFirewall = true;
-  };
-
   # Enable sound with pipewire.
   # sound.enable = true;
   hardware.pulseaudio.enable = false;
@@ -116,22 +68,8 @@
     #media-session.enable = true;
   };
 
-  ## Ollama
-  #services.ollama = {
-    ##package = pkgs.unstable.ollama; # Uncomment if you want to use the unstable channel, see https://fictionbecomesfact.com/nixos-unstable-channel
-    #enable = true;
-    #acceleration = "cuda"; # Or "rocm"
-    ##environmentVariables = { # I haven't been able to get this to work myself yet, but I'm sharing it for the sake of completeness
-      ## HOME = "/home/ollama";
-      ## OLLAMA_MODELS = "/home/ollama/models";
-      ## OLLAMA_HOST = "0.0.0.0:11434"; # Make Ollama accesible outside of localhost
-      ## OLLAMA_ORIGINS = "http://localhost:8080,http://192.168.0.10:*"; # Allow access, otherwise Ollama returns 403 forbidden due to CORS
-    ##};
-  #};
-  
-
   # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
+  services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
   users.users.bear = {
@@ -160,47 +98,19 @@
      wget
      curl
      pkgs.bitwarden
-     pkgs.nextcloud-client
      terminator
-     # vimplugin-vim-terminator
      pkgs.gnome-tweaks
      pkgs.gnome.gnome-boxes
-     pkgs.tor-browser
      pkgs.cherrytree
      pkgs.git
-     pkgs.sparrow
-     pkgs.spotify
      pkgs.btop
-     pkgs.remmina
-     pkgs.openvpn
-     pkgs.vscode
-     #pkgs.zsh
      pkgs.neofetch
      pkgs.neovim
      pkgs.starship
      pkgs.autojump
-     pkgs.libreoffice
-     pkgs.freetube 
-     # Ollama related package below
-     pkgs.oterm
-     pkgs.llama-cpp
-     pkgs.ollama
-     
      pkgs.chromium
-     pkgs.flameshot
-     pkgs.python3
-     #pkgs.mullvad
-     #pkgs.mullvad-browser
-     pkgs.qbittorrent
-     pkgs.jellyfin
-     pkgs.jellyfin-web
-     pkgs.jellyfin-ffmpeg
-     pkgs.vlc
-     pkgs.virt-manager
      pkgs.libvirt
      pkgs.qemu
-     pkgs.gnomeExtensions.openweather-refined
-
      #RDP session package
      pkgs.gnome.gnome-session
 
@@ -223,23 +133,8 @@
     MOZ_ENABLE_WAYLAND = 0;
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.steam.enable = true
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-  
-  # To Enable Steam Games
-  programs.steam = {
-  enable = true;
-  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-  };
-  
-  # xRDP SEssion Gnome enable
+ 
+   # xRDP SEssion Gnome enable
   services.xrdp = {
     enable = true;
     openFirewall = true;
@@ -248,11 +143,6 @@
 
   # Enable the OpenSSH daemon.
   # services.openssh.enable = true;
-
-  # Enable tor  
-    services.tor.client.enable = true;
-  # Enable Mullvad service
-    # services.mullvad-vpn.enable = true;
 
   # Virt-Manager service
   virtualisation.libvirtd.enable = true;
